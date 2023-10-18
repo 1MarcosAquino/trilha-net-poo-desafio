52051e5af48372b6864cffdeefe7e53191ec4c3a
1MarcosAquino <1marcosaquino@gmail.com>
2023-10-18 13:04:57 -0300

    completed

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,37 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando...");
         }
-
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação...");
+        }
+
+        // tomei a liberdade de criar esse metodo estou ciente de que isso modifica o diagrama.
+        // É que eu não vi o uso dessas propriedades. espero não ter feito besteira.  
+        public void About()
+        {
+            Console.WriteLine("Sobre");
+            Console.Write($"Modelo: {Modelo}, ");
+            Console.Write($"IMEI: {IMEI}, ");
+            Console.WriteLine($"Memoria: {Memoria}GB.");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Y29pc2FzIGVzdHJhbmhlcw== está em Base64 e significa coisas estranhas
+// bW9udGUgZGUgY29pc2FzIGVzdHJhbmhhcw== está em Base64 e significa monte de coisas estranhas
+// Coloquei assim porque achei que ficaria legal :D
+
+Nokia cel1 = new Nokia("999", "Nokia-X", "Y29pc2FzIGVzdHJhbmhlcw==", 16);
+Iphone cel2 = new Iphone("888", "Iphone-20", "bW9udGUgZGUgY29pc2FzIGVzdHJhbmhhcw==", 32);
+cel1.Ligar();
+cel1.InstalarAplicativo("Linkedin");
+cel1.About();
+
+cel2.Ligar();
+cel2.InstalarAplicativo("DIO");
+cel2.About();
